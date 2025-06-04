a228807c7a99c0f140e0323354d75b9b683ec711
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-04 20:11:21 +0530

    expo setup for tailwind and ios build

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-red-800">HI! Welcome</Text>
+      <Text className="text-5xl text-red-800 font-bold">HI! Welcome</Text>
     </View>
   );
 }
