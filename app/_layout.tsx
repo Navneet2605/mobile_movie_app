03dfaa7b96596147cd94326fa9a3442935f02f18
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-06 15:49:07 +0530

    remove the battery and time status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack >
+  return(
+    <>
+    <StatusBar hidden={true}/>
+    <Stack >
     <Stack.Screen
     name="(tabs)"
     options={{headerShown:false,}}/>
@@ -10,4 +14,7 @@ export default function RootLayout() {
     name="movies/[id]"
     options={{headerShown:false,}}/>
   </Stack>
+    </>
+  )
+   
 }
